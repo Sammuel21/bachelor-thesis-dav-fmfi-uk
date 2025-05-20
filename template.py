f5f4ac3a5cda44cacdb2b8dc8ee59cce3b909af1
Samuel Gulas <gulas16@uniba.sk>
2025-05-20 15:02:26 +0200

    first commit; add project structure

---
+++ template.py
@@ -0,0 +1,28 @@
+# NOTE: JSON API request template:
+# Processed by cqgram to create cq data
+
+request = {
+    'source' : 'Yahoo',
+
+    'content' : {
+        'start' : None,
+        'end' : None,
+
+        'tau_x_list' : [],
+        'tau_y_list' : [],
+
+        'data' : {
+            # NOTE:
+            # pass key, value pairs of assets, tickers HERE
+        }
+    }
+}
+
+
+# NOTE: zoznam podporovanych data sources:
+
+# Yahoo:    YES
+# Polygon:  NO
+# EODH:     NO
+
+
